8be89ad0733bddbbeda1aeffa29ebc75c566dbef
HenriS67 <henrib2001@gmail.com>
2023-05-09 10:03:48 +0200

    gg

--- video/MotionTrackingTraining.py
+++ video/MotionTrackingTraining.py
@@ -30,6 +30,27 @@ tableauSortieJBas=[]
 print(tableauSortieJHaut)
 
 tabCoups=["problemeDetection/PasClair...=>Poubelle","coup droit","revers","deplacement","service","immobile"]
+
+########################TRAINING:
+
+from pynput import keyboard
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False  # stop listener
+    try:
+        k = key.char  # single-char keys
+    except:
+        k = key.name  # other keys
+    if k in ['space']:  # keys of interest
+        # self.keys.append(k)  # store it in global-like variable
+        print('Key pressed: ' + k)
+        return False  # stop listener; remove this if want more keys
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()  # start to listen on a separate thread
+listener.join()  # remove if main thread is polling self.keys
+
 ########################METHODES TRAITEMENT CONTOURS :
 
 def aire(rec):
