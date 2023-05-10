166ceed4a3577a1ff60c2a978ddb0e7e2680a494
HenriS67 <henrib2001@gmail.com>
2023-05-10 12:38:33 +0200

    recenterImage

---
+++ video/recenterImage.py
@@ -0,0 +1,33 @@
+import cv2
+#!pip install .
+import cv2
+import numpy as np
+from smart.processor import ImageProcessor
+from smart.video import Video, Image
+from pynput.keyboard import Key, Listener
+from pynput import keyboard
+from matplotlib import pyplot as plt
+
+import os
+ 
+directory = 'images'
+images=[]
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        print(f)
+
+        img = cv2.imread(f)
+        imp=ImageProcessor()
+        grayimg=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img_out=imp.crop_silouhette(grayimg,50)
+        images.append(img_out)
+
+
+cv2.imshow('Gray image', grayimg)
+  
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+imp.save_ImageList(images, directory, True)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
