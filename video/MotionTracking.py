50c21e9f9dbcecea13001d46368aa7775abe68e7
HenriS67 <henrib2001@gmail.com>
2023-05-11 20:57:13 +0200

    fix

--- video/MotionTracking.py
+++ video/MotionTracking.py
@@ -24,7 +24,7 @@ devMode=True#mode Développeur (=voir les tous les contours, filtres...)
 affichage=True#est-ce qu'on veut afficher les resultats ou juste enregistrer ?
 enregistrementImage=True#Est-ce qu'on veut enregistrer la sortie en image ou juste en tableau de 0 et de 1
 PixelSizeOutput=100#taille de la sortie (=entree du machine learning)
-videoPath='dataset/services.mp4'#chemin de la video
+videoPath='dataset/clip/2.mp4'#chemin de la video
 fpsOutput=7#FPS de la sortie
 videoResize=(800,400)#taille pour resize de la video pour traitement (petite taille = plus rapide) 
 cutFrameNB=15#nombre d'images pour un coups
@@ -122,7 +122,7 @@ imageProcessor = ImageProcessor()
 ret3, frame3 = cap.read()
 #####AJUSTEMENT TAILLE
 frame1=cv2.resize(frame3,videoResize)
-milieu_y=200
+milieu_y=150
 milieu_x=400
 
 #####INIT CONTOURS JOUEURS AU MILIEU DU TERRAIN (joeur 0 = joueur du haut, joueur 1 = joueur du bas)
@@ -201,7 +201,7 @@ while cap.isOpened() and ret3:#attention video qui s'arete au premier probleme d
         if area > 1:
             x, y, w, h = cv2.boundingRect(contour)
             if area > 2000 : continue
-            if area > 300:
+            if area > 300 and not  (w/h>4 or h/w>4):
                 tab_rec.append((x, y, w, h))
             else:
                 ball_rec.append((x, y, w, h))
@@ -362,18 +362,21 @@ while cap.isOpened() and ret3:#attention video qui s'arete au premier probleme d
         # silouhetteHaut = imageProcessor.resize_img(crop_imgHaut,(50, 50), interpolation=cv2.INTER_LINEAR)  
         # silouhetteBas = imageProcessor.resize_img(crop_imgBas, (50, 50), interpolation=cv2.INTER_LINEAR)
         
-        silouhetteBas  = transformations[3][joueurs[1][1]-15:joueurs[1][1] + joueurs[1][3]+30,joueurs[1][0]-15:joueurs[1][0] + joueurs[1][2]+30]
+        silouhetteBas  = transformations[3][max(0,joueurs[1][1]-15):joueurs[1][1] + joueurs[1][3]+30,max(0,joueurs[1][0]-15):joueurs[1][0] + joueurs[1][2]+30]
+        
         silouhetteBas=np.ceil(silouhetteBas/255)*255
         silouhetteBas=util.filter(silouhetteBas, "closing",parameters_silouhette["filter"])
         silouhetteBas=cv2.resize(silouhetteBas,(PixelSizeOutput,PixelSizeOutput))
+    except:
+        silouhetteBas = np.zeros((PixelSizeOutput,PixelSizeOutput))
 
-        silouhetteHaut = transformations[3][joueurs[0][1]-15:joueurs[0][1] + joueurs[0][3]+30,joueurs[0][0]-15:joueurs[0][0] + joueurs[0][2]+30]
+    try:
+        silouhetteHaut = transformations[3][max(joueurs[0][1]-15,0):joueurs[0][1] + joueurs[0][3]+30,max(0,joueurs[0][0]-15):joueurs[0][0] + joueurs[0][2]+30]
         silouhetteHaut=np.ceil(silouhetteHaut/255)*255
         silouhetteHaut=util.filter(silouhetteHaut, "closing",parameters_silouhette["filter"])
         silouhetteHaut=cv2.resize(silouhetteHaut,(PixelSizeOutput,PixelSizeOutput))
     except:
         silouhetteHaut = np.zeros((PixelSizeOutput,PixelSizeOutput))
-        silouhetteBas = np.zeros((PixelSizeOutput,PixelSizeOutput))
 
     ###ENREGISTREMENT des silouhettes dans le TABLEAU
     tableauSortieJHaut.append(silouhetteHaut/255)
